7e3c3e608887bfe17a6352bfca65c5bb7dd6fb8a
Caballero Villavicencio Francisco <105505239+CaballeroVillavicencioFranciscoDSM33@users.noreply.github.com>
2025-05-28 19:39:53 -0600

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'auth' => \App\Http\Middleware\CustomAuthenticate::class,
-        ]);
-    })
+    ->withMiddleware(function (Middleware $middleware) {})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
